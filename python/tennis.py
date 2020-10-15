88e3d888ebb43fd3addefeaa13bf2eb749023184
dreddick.home2@gmail.com <dreddick.home2@gmail.com>
2020-10-15 13:00:45 +0100

    1st refactor

--- python/tennis.py
+++ python/tennis.py
@@ -7,24 +7,27 @@ class TennisGame1:
         self.player2Name = player2Name
         self.p1points = 0
         self.p2points = 0
+        self.minusResult = 0
         
     def won_point(self, playerName):
         if playerName == self.player1Name:
             self.p1points += 1
         else:
             self.p2points += 1
+        self.minusResult = self.p1points-self.p2points
     
     def score(self):
         result = ""
         tempScore=0
         if (self.p1points==self.p2points):
-            result = {
+            return {
                 0 : "Love-All",
                 1 : "Fifteen-All",
                 2 : "Thirty-All",
             }.get(self.p1points, "Deuce")
-        elif (self.p1points>=4 or self.p2points>=4):
-            minusResult = self.p1points-self.p2points
+
+        if (self.p1points>=4 or self.p2points>=4):
+            minusResult = self.minusResult
             if (minusResult==1):
                 result ="Advantage " + self.player1Name
             elif (minusResult ==-1):
@@ -33,19 +36,20 @@ class TennisGame1:
                 result = "Win for " + self.player1Name
             else:
                 result ="Win for " + self.player2Name
-        else:
-            for i in range(1,3):
-                if (i==1):
-                    tempScore = self.p1points
-                else:
-                    result+="-"
-                    tempScore = self.p2points
-                result += {
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
-                }[tempScore]
+            return result
+
+        for i in range(1,3):
+            if (i==1):
+                tempScore = self.p1points
+            else:
+                result+="-"
+                tempScore = self.p2points
+            result += {
+                0 : "Love",
+                1 : "Fifteen",
+                2 : "Thirty",
+                3 : "Forty",
+            }[tempScore]
         return result
 
 
